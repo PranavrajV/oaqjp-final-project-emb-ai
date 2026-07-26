3c6b88f8cb81f0828044c9e103278b81f0932f11
PranavrajV <prjv9000@gmail.com>
2026-07-26 13:26:22 -0400

    commit 2

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,18 @@
+import requests, json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    input_jsonobj = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json = input_jsonobj, headers = headers)
+
+    formatted_response = json.loads(response.text)
+
+    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
+
+    emotion_scores['dominant_emotion'] = max(emotion_scores, key=emotion_scores.get)
+
+    return emotion_scores
